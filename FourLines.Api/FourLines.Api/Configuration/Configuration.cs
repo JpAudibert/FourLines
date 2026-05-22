5da2c46f130ec4d80e3fabbf5a2411d1022a5d5c
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-05-21 22:25:45 -0300

    updates collections positioning

---
+++ FourLines.Api/FourLines.Api/Configuration/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace FourLines.Api.Configuration;
+
+public static class Configuration
+{
+    public static string JwtKey { get; set; } = "18a88070-a221-4662-a157-5e1a802edb18";
+}
---
+++ FourLines.Api/FourLines.Api/Configuration/TokenService.cs
@@ -0,0 +1,5 @@
+﻿namespace FourLines.Api.Configuration;
+
+public class TokenService
+{
+}
