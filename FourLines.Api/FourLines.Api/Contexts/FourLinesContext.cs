04f934e489353a5cc932f52b8e0add86f7dfdbe3
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-05-27 20:58:50 -0300

    feature: updates database context

--- FourLines.Api/FourLines.Api/Contexts/FourLinesContext.cs
+++ FourLines.Api/FourLines.Api/Contexts/FourLinesContext.cs
@@ -1,4 +1,4 @@
-﻿ using FourLines.Api.DataModels;
+﻿using FourLines.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FourLines.Api.Contexts;
@@ -18,29 +18,7 @@ public class FourLinesContext(DbContextOptions<FourLinesContext> options) : DbCo
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>()
-        .HasOne(user => user.Role)
-        .WithMany(roles => roles.Users)
-        .HasForeignKey(u => u.RoleId)
-        .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Facility>()
-        .HasOne(facility => facility.Owner)
-        .WithMany(user => user.Facilities)
-        .HasForeignKey(f => f.OwnerId)
-        .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Court>()
-        .HasOne(court => court.Sport)
-        .WithMany(sport => sport.Courts)
-        .HasForeignKey(c => c.SportId)
-        .OnDelete(DeleteBehavior.Cascade);
-
-        modelBuilder.Entity<Court>()
-        .HasOne(court => court.Facility)
-        .WithMany(facility => facility.Courts)
-        .HasForeignKey(c => c.FacilityId)
-        .OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FourLinesContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
     }
