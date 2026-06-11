c45565989e32d7ac94a9dc9de58efe61c0412229
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-06-10 22:32:56 -0300

    feature: removes files that are not from API project

--- FourLines.Api/FourLines.Api/Models/Role.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FourLines.Api.Models;
-
-public class Role : BaseEntity
-{
-    public string Name { get; init; } = default!;
-
-    public IEnumerable<User> Users { get; init; } = [];
-
-}
