661c25167394d693921aa5cc1d17b02a1f430f77
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-07-01 00:12:13 -0300

    tests: fix to use in memory

--- FourLines.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ FourLines.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using FourLines.Infrastructure.Contexts;
 using FourLines.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace FourLines.Infrastructure.DependencyInjection;
 
@@ -9,9 +11,19 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<FourLinesContext>(options =>
-            options
-                .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
-                .UseSnakeCaseNamingConvention());
+        {
+            // Allow tests to opt-in to an in-memory provider using configuration
+            if (configuration.GetValue<bool>("UseInMemory", false))
+            {
+                options.UseInMemoryDatabase("FourLines_InMemory_Db");
+            }
+            else
+            {
+                options
+                    .UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                    .UseSnakeCaseNamingConvention();
+            }
+        });
 
         services.AddScoped(typeof(IStandardRepository<>), typeof(StandardRepository<>));
 
