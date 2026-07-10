fb2402aed2b773c4b2ab44d3614da59384174f4c
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-07-10 13:30:53 -0300

    feat: configures Serilog and OpenTelemetry

--- FourLines.Api/Program.cs
+++ FourLines.Api/Program.cs
@@ -1,81 +1,78 @@
+using FourLines.Api.DependencyInjection;
+using FourLines.Api.Extensions;
+using Serilog;
 
+const string appName = "FourLines";
+const string appVersion = "1.0.0";
 
-using Microsoft.AspNetCore.Http.Features;
-using System.Diagnostics;
+const string openTelemetryEndpoint = "http://localhost:4317";
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-var secretKey = jwtSettings["Secret"];
-
-// Add services to the container.
-
-builder.Services
-    .AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-    });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services
-    .AddDomain()
-    .AddApplication()
-    .AddInfrastructure(builder.Configuration);
-
-builder.Services.AddApiVersioning(options =>
+try
 {
-    options.DefaultApiVersion = new ApiVersion(1, 0);
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ReportApiVersions = true;
-});
+    Log.Information("Starting application");
 
-builder.Services
-    .AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.ConfigureSerilog(appName, appVersion, openTelemetryEndpoint);
+    builder.Services.ConfigureOpenTelemtryTracingAndMetrics(openTelemetryEndpoint);
+
+    var jwtSettings = builder.Configuration.GetSection("Jwt");
+    var secretKey = jwtSettings["Secret"];
+
+    // Add services to the container.
+    builder.Services
+        .AddControllers()
+        .AddJsonOptions(options =>
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings["Issuer"],
-            ValidAudience = jwtSettings["Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey!))
-        };
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddOpenApi();
+
+    builder.Services
+        .AddDomain()
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
     });
 
-builder.Services.AddProblemDetails(options =>
-{
-    options.CustomizeProblemDetails = (context) =>
+    builder.Services.ConfigureJwtAuthentication(jwtSettings["Issuer"]!, jwtSettings["Audience"]!, secretKey!);
+
+    builder.Services.ConfigureProblemDetails();
+
+    builder.Services.AddAuthorization();
+
+    var app = builder.Build();
+
+    app.ConfigureSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
-        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+        app.MapOpenApi();
+        app.MapScalarApiReference(options => options.AddDocument("v1", "API Version 1", isDefault: true));
+    }
 
-        Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
-        context.ProblemDetails.Extensions.TryAdd("traceId", activity?.TraceId.ToString() ?? context.HttpContext.TraceIdentifier);
-    };
-});
+    app.UseAuthorization();
 
-builder.Services.AddAuthorization();
+    app.MapControllers();
 
-var app = builder.Build();
+    Log.Information("Application started successfully");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference(options => options.AddDocument("v1", "API Version 1", isDefault: true));
+    app.Run();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
