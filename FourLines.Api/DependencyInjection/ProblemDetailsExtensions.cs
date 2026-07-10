ac5b5bf04874005df52cfb9a72f2f84538adf1e2
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-07-10 13:30:53 -0300

    feat: configures Serilog and OpenTelemetry

---
+++ FourLines.Api/DependencyInjection/ProblemDetailsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using System.Diagnostics;
+
+namespace FourLines.Api.DependencyInjection;
+
+public static class ProblemDetailsExtensions
+{
+    public static IServiceCollection ConfigureProblemDetails(this IServiceCollection services)
+    {
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = (context) =>
+            {
+                context.ProblemDetails.Instance = $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+                context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+
+                Activity? activity = context.HttpContext.Features.Get<IHttpActivityFeature>()?.Activity;
+                context.ProblemDetails.Extensions.TryAdd("traceId", activity?.TraceId.ToString() ?? context.HttpContext.TraceIdentifier);
+            };
+        });
+
+        return services;
+    }
+}
