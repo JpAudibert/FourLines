71d63831300117058ade4604f316b0ee72229cdf
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-08-31 16:35:08 -0300

    tests: create connection and update data sources

--- FourLines.Tests/Shared/InMemoryDataSource.cs
+++ FourLines.Tests/Shared/InMemoryDataSource.cs
@@ -5,11 +5,21 @@ namespace FourLines.Tests.Shared;
 
 public class InMemoryDataSource
 {
-    public readonly static DateTimeOffset DateTime = DateTimeOffset.Now;
-    public readonly static Role RoleOwner = new() { Name = RoleConstants.FacilityOwner };
-    public readonly static Role RolePlayer = new() { Name = RoleConstants.Player };
+    public static readonly Guid RoleOwnerId = new("17b044a4-3c53-4fc9-86b1-16f314877de0");
+    public static readonly Guid RolePlayerId = new("76199b24-62ef-49f1-816e-decfa45c2900");
+    public static readonly Guid UserOwnerId = new("e7021ffc-ab35-4475-b640-a6b23742a132");
+    public static readonly Guid UserPlayerId = new("1a8997ec-171f-4af2-82b1-49a914b2a526");
+    public static readonly Guid FacilityId = new("40b43ff5-2ad0-4a82-b7df-574f2f7ca716");
+    public static readonly Guid TestSportId = new("ad4eb568-bbe0-43fe-a6ed-3e1bb1e05340");
+    public static readonly Guid CourtId = new("1c07cc5e-036e-4d23-a3ed-ac8d64b2968c");
+    public static readonly Guid FacilityScheduleId = new("26a011e8-25a8-4b91-8471-0c4f1854eee4");
 
-    public readonly static User UserOwner = new()
+    public static readonly DateTimeOffset DateTime = DateTimeOffset.Now;
+    public static readonly DateTimeOffset SettedDateTime = new(2026, 8, 31, 10, 0, 0, TimeSpan.Zero);
+    public static readonly Role RoleOwner = new() { Name = RoleConstants.FacilityOwner };
+    public static readonly Role RolePlayer = new() { Name = RoleConstants.Player };
+
+    public static readonly User UserOwner = new()
     {
         RoleId = RoleOwner.Id,
         Name = "John Doe",
@@ -20,7 +30,7 @@ public class InMemoryDataSource
         RegistrationNumber = "383.975.210-89",
     };
 
-    public readonly static User UserPlayer = new()
+    public static readonly User UserPlayer = new()
     {
         RoleId = RolePlayer.Id,
         Name = "Jane Smith",
@@ -31,7 +41,7 @@ public class InMemoryDataSource
         RegistrationNumber = "383.975.211-89",
     };
 
-    public readonly static Facility Facility1 = new()
+    public static readonly Facility Facility1 = new()
     {
         Name = "Test Facility 1",
         Address = "123 Test St",
@@ -42,7 +52,7 @@ public class InMemoryDataSource
         OwnerId = UserOwner.Id,
     };
 
-    public readonly static Facility Facility2 = new()
+    public static readonly Facility Facility2 = new()
     {
         Name = "Test Facility 2",
         Address = "456 Test Ave",
@@ -53,7 +63,7 @@ public class InMemoryDataSource
         OwnerId = UserOwner.Id,
     };
 
-    public readonly static Sport TestSport = new()
+    public static readonly Sport TestSport = new()
     {
         Name = "Test Sport",
         Indoor = true,
@@ -61,7 +71,7 @@ public class InMemoryDataSource
         MaxPlayersCount = 10,
     };
 
-    public readonly static Court Court1 = new()
+    public static readonly Court Court1 = new()
     {
         FacilityId = Facility1.Id,
         SportId = TestSport.Id,
@@ -69,7 +79,7 @@ public class InMemoryDataSource
         IsActive = true,
     };
 
-    public readonly static Court Court2 = new()
+    public static readonly Court Court2 = new()
     {
         FacilityId = Facility1.Id,
         SportId = TestSport.Id,
@@ -77,7 +87,7 @@ public class InMemoryDataSource
         IsActive = true,
     };
 
-    public readonly static FacilitySchedule FacilitySchedule1 = new()
+    public static readonly FacilitySchedule FacilitySchedule1 = new()
     {
         FacilityId = Facility1.Id,
         DayOfWeek = DayOfWeek.Tuesday,
@@ -85,7 +95,7 @@ public class InMemoryDataSource
         ClosesAt = new TimeOnly(20, 0),
     };
 
-    public readonly static FacilitySchedule FacilitySchedule2 = new()
+    public static readonly FacilitySchedule FacilitySchedule2 = new()
     {
         FacilityId = Facility1.Id,
         DayOfWeek = DayOfWeek.Thursday,
@@ -93,7 +103,7 @@ public class InMemoryDataSource
         ClosesAt = new TimeOnly(20, 0),
     };
 
-    public readonly static FacilitySchedule FacilitySchedule3 = new()
+    public static readonly FacilitySchedule FacilitySchedule3 = new()
     {
         FacilityId = Facility1.Id,
         DayOfWeek = DateTimeOffset.Now.DayOfWeek,
@@ -101,7 +111,7 @@ public class InMemoryDataSource
         ClosesAt = new TimeOnly(23, 59),
     };
 
-    public readonly static Reservation Reservation1 = new()
+    public static readonly Reservation Reservation1 = new()
     {
         CourtId = Court1.Id,
         UserId = UserPlayer.Id,
@@ -109,7 +119,7 @@ public class InMemoryDataSource
         Status = ReservationStatus.Pending,
     };
 
-    public readonly static Reservation Reservation2 = new()
+    public static readonly Reservation Reservation2 = new()
     {
         CourtId = Court1.Id,
         UserId = UserPlayer.Id,
@@ -117,7 +127,7 @@ public class InMemoryDataSource
         Status = ReservationStatus.Pending,
     };
 
-    public readonly static Reservation Reservation3 = new()
+    public static readonly Reservation Reservation3 = new()
     {
         CourtId = Court2.Id,
         UserId = UserPlayer.Id,
---
+++ FourLines.Tests/Shared/PostgresTestDatabase.cs
@@ -0,0 +1,62 @@
+﻿using FourLines.Domain.Models;
+using FourLines.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace FourLines.Tests.Shared;
+
+public class PostgresTestDatabase : IAsyncLifetime
+{
+    private readonly IConfiguration _configuration;
+
+    public PostgresTestDatabase()
+    {
+        _configuration = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.Tests.json")
+            .AddInMemoryCollection()
+            .Build();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await using var context = CreateContext();
+        await context.Database.EnsureDeletedAsync();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await using var context = CreateContext();
+
+        await context.Database.MigrateAsync();
+    }
+
+    public FourLinesContext CreateContext()
+    {
+        string connectionString =
+            Environment.GetEnvironmentVariable("ConnectionStrings__Postgres") ??
+            _configuration.GetConnectionString("DefaultConnection") ??
+            throw new InvalidOperationException("Connection string not found.");
+
+        DbContextOptions<FourLinesContext> options = new DbContextOptionsBuilder<FourLinesContext>()
+            .EnableDetailedErrors()
+            .UseNpgsql(connectionString)
+            .UseSnakeCaseNamingConvention()
+            .Options;
+
+        return new FourLinesContext(options);
+    }
+
+    public static async Task<T> CreateEntityInMemory<T>(T entity, FourLinesContext pgContext)
+        where T : BaseEntity
+    {
+        if (await pgContext.FindAsync<T>(entity.Id) == null)
+        {
+            await pgContext.Set<T>().AddAsync(entity);
+            await pgContext.SaveChangesAsync();
+        }
+
+        return entity;
+    }
+}
