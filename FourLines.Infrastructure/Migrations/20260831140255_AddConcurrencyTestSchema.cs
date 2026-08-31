2b3e03fe31e96e80fb4aa9186d7f52bafd0dc486
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-08-31 16:34:03 -0300

    feat: add migration for tests

---
+++ FourLines.Infrastructure/Migrations/20260831140255_AddConcurrencyTestSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FourLines.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConcurrencyTestSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
