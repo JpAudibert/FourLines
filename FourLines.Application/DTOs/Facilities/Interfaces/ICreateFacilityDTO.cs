5345e35976612058d6eea9e2df8e3a0a4a778ac3
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-09-09 13:47:17 -0300

    feat: updates facility's DTOs to use interfaces

---
+++ FourLines.Application/DTOs/Facilities/Interfaces/ICreateFacilityDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FourLines.Application.DTOs.Facilities.Interfaces
+{
+    public interface ICreateFacilityDTO
+    {
+        string Address { get; init; }
+        string City { get; init; }
+        string Name { get; init; }
+        Guid OwnerId { get; init; }
+        string RegistrationNumber { get; init; }
+        string State { get; init; }
+        string ZipCode { get; init; }
+    }
+}
---
+++ FourLines.Application/DTOs/Facilities/Interfaces/IDeleteFacilityDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace FourLines.Application.DTOs.Facilities.Interfaces
+{
+    public interface IDeleteFacilityDTO
+    {
+        Guid FacilityId { get; init; }
+        Guid OwnerId { get; init; }
+    }
+}
