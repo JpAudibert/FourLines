7da115c7f73afe14e5ca3c736f0704abdae09a7d
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-09-14 08:54:09 -0300

    ✅ adds tests for getting a match

--- FourLines.Tests/Matches/TestMatchesRead.cs
+++ FourLines.Tests/Matches/TestMatchesRead.cs
@@ -1,4 +1,47 @@
-namespace Name
+using FourLines.Application.Interfaces;
+using FourLines.Domain.Results;
+using FourLines.Tests.Shared;
+using FourLines.Domain.Models;
+
+namespace FourLines.Tests.Matches;
+
+[Collection(FourLinesCollection.Name)]
+public class TestMatchesRead(FourLinesFixture fixtures)
 {
-    
+    [Fact]
+    public async Task Should_GetMatch()
+    {
+        // Arrange
+        await using var scope = fixtures.CreateAsyncServiceScope();
+        await fixtures.EnsureGoalKeeperReservationCreatedAsync();
+
+        Guid matchId = fixtures.GoalKeeperReservationResult.Value.Match.Id;
+
+        IMatchHandler handler = fixtures.ServiceProvider.GetRequiredService<IMatchHandler>();
+
+        // Act
+        Result<Match?> result = await handler.GetMatch(matchId);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.NotNull(result.Value);
+        Assert.Equal(matchId, result.Value.Id);
+    }
+
+    [Fact]
+    public async Task Should_GetMatch_Empty()
+    {
+        // Arrange
+        await using var scope = fixtures.CreateAsyncServiceScope();
+
+        Guid matchId = Guid.NewGuid();
+
+        IMatchHandler handler = fixtures.ServiceProvider.GetRequiredService<IMatchHandler>();
+
+        // Act
+        Result<Match?> result = await handler.GetMatch(matchId);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
 }
