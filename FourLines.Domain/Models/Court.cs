d22f81c85d8f58ca8133d0b5e4a1993fbbc178aa
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-09-15 09:16:05 -0300

    🎨 Adds renting and maintenance period to courts

--- FourLines.Domain/Models/Court.cs
+++ FourLines.Domain/Models/Court.cs
@@ -7,6 +7,8 @@ public record Court : BaseEntity
     public string Name { get; init; } = default!;
     public bool IsActive { get; init; }
     public Money DefaultPrice { get; init; } = default!;
+    public int RentingPeriodInMinutes { get; init; }
+    public int MaintenancePeriodInMinutes { get; init; }
 
     public Sport Sport { get; init; } = default!;
     public Facility Facility { get; init; } = default!;
