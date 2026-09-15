df4e80175c7a3b9d0a16f8dbcfd4effd25786f3d
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-09-15 09:06:06 -0300

    🎨 Adds default price to Court and price for the Reservation

--- FourLines.Domain/Models/Court.cs
+++ FourLines.Domain/Models/Court.cs
@@ -6,6 +6,7 @@ public record Court : BaseEntity
     public Guid SportId { get; init; }
     public string Name { get; init; } = default!;
     public bool IsActive { get; init; }
+    public Money DefaultPrice { get; init; } = default!;
 
     public Sport Sport { get; init; } = default!;
     public Facility Facility { get; init; } = default!;
