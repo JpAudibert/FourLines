55df9ab73ba913f040e8dbc880f71f6e5d0abda3
João Pedro Basso Audibert <hotsjoao@gmail.com>
2026-09-17 16:20:26 -0300

    ✨ Adds shuffling handler

---
+++ FourLines.Application/Handlers/ShuffleHandler.cs
@@ -0,0 +1,44 @@
+namespace FourLines.Application.Handlers;
+
+public class ShuffleHandler(FourLinesContext context, IShuffleStrategy shuffleStrategy)
+    : IShuffleHandler
+{
+    public async Task<Result<IEnumerable<MatchesUsers>>> ShufflePlayers(Guid matchId)
+    {
+        IQueryable<MatchesUsers> players = context.MatchesUsers.Where(mu => mu.MatchId == matchId);
+        if (!players.Any())
+            return Result<IEnumerable<MatchesUsers>>.Failure(
+                ShufflingErrorResults.ShufflingUnknownMatch
+            );
+
+        IQueryable<MatchesUsers> fixedPositions = players.Where(p => p.IsFixedPosition);
+        IQueryable<MatchesUsers> notFixedPositions = players.Where(p => !p.IsFixedPosition);
+
+        IEnumerable<MatchesUsers> shuffledFixedPositions = shuffleStrategy.Shuffle(fixedPositions);
+        IEnumerable<MatchesUsers> shuffledNotFixedPositions = shuffleStrategy.Shuffle(
+            notFixedPositions
+        );
+
+        List<MatchesUsers> shuffledPlayers =
+        [
+            .. shuffledFixedPositions,
+            .. shuffledNotFixedPositions,
+        ];
+
+        for (int i = 0; i < shuffledPlayers.Count; i++)
+        {
+            shuffledPlayers[i].TeamNumber = i % 2 == 0 ? 1 : 2;
+        }
+
+        using var transaction = await context.Database.BeginTransactionAsync();
+
+        await context.MatchesUsers.Where(mu => mu.MatchId == matchId).ExecuteDeleteAsync();
+
+        await context.AddRangeAsync(shuffledPlayers);
+        await context.SaveChangesAsync();
+
+        await transaction.CommitAsync();
+
+        return Result<IEnumerable<MatchesUsers>>.Success(shuffledPlayers);
+    }
+}
